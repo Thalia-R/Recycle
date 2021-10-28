87eed2b64965c0b38f48d81d0e23107e566b0d3f
Thalia-R <56529946+Thalia-R@users.noreply.github.com>
2021-10-28 15:20:49 +0200

    Add files via upload

---
+++ js/navigation.js
@@ -0,0 +1,8 @@
+function smallNavigation() {
+    var x = document.getElementById("nav");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
